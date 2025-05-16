dc33dd63b7f5c901d9971dac20d683cdf63bc59f
deepankar-MT24031 <deepankar24031@iiitd.ac.in>
2025-05-16 15:37:43 -0700

    lmego

--- pdf_generator.py
+++ pdf_generator.py
@@ -504,7 +504,7 @@ def generate_pdf_from_latex(heading, subheading, patient_info, treatment_tables,
 \\
 \noindent\begin{{tabular}}{{|p{{11.3cm}}|p{{7.3cm}}|}}
      \hline
-    \textbf{{Consultant:}} {patient_info["consultant_names"]}      & \textbf{{JRs:}} {patient_info["jr_names"]} \\
+    \textbf{{Consultant:}} \parbox[t]{{10.5cm}}{{{patient_info["consultant_names"]}}}      & \textbf{{JRs:}} {patient_info["jr_names"]} \\
     \cline{{2-2}}
       & \textbf{{SRs:}} {patient_info["sr_names"]} \\
     \hline
